c5a7b198ab7edff894405cfa8c29eb1584d0e44c
Peter H <wappospypuppy@gmail.com>
2023-07-03 14:32:53 -0700

    Instant simulation

--- testsite.js
+++ testsite.js
@@ -9,6 +9,7 @@ const kDCheck = document.getElementById('kD-check');
 const biasSlider = document.getElementById('bias-slide');
 const biasCheck = document.getElementById('bias-check');
 
+const instChecked = document.getElementById('inst-check');
 
 const cfg = {
   type: 'line',
@@ -37,6 +38,8 @@ let cht = new Chart(ctx, cfg);
 
 let intervalSeconds = 0.016;
 const intervalMillis = 1000 * intervalSeconds;
+const fullTimeDT = 0.016;
+const fullTimeEnd = 25;
 
 const maxKP = 2;
 const maxKI = 1;
@@ -55,19 +58,44 @@ let accErr = 0;
 
 let vel = 0;
 
+let instID = 0;
+
 let dateTime = Date.now();
 
 myReset();
+instChecked.checked = false;
+
+    function simulateFullTime() {
+        t = 0;
+        let dt = fullTimeDT;
+
+        myReset(false);
+        for (t = 0; t < fullTimeEnd; t += dt) {
+            step(dt);
+        }
+        cht.update('none');
+    }
+
+    function instToggle() {
+        if (instChecked.checked) {
+            myReset();
+            instID = setInterval(simulateFullTime, 100);
+            return;
+        }
+
+        //myReset();
+        clearInterval(instID);
+    }
 
 
     function addOne() {
+        
+
         let newTime = Date.now();
-        intervalSeconds = (newTime - dateTime) / 1000;
+        let dt = (newTime - dateTime) / 1000;
         dateTime = newTime;
 
-        cht.data.datasets[0].data.push({x: t, y: err});
-        systemUpdate();
-        t += intervalSeconds;
+        step(dt);
         cht.update('none');
 
         if (t >= 100) {
@@ -75,18 +103,26 @@ myReset();
         }
     }
 
+    function step(dt) {
+        cht.data.datasets[0].data.push({x: t, y: err});
+        systemUpdate(dt);
+        t += dt;
+    }
+
     function stop() {
         clearInterval(addId);
     }
 
-    function myReset() {
+    function myReset(update=true) {
         stop();
         t = 0;
         err = -15;
         oldErr = err;
         accErr = 0;
         cht.data.datasets[0].data = [];
-        cht.update();
+        if (update) {
+            cht.update();
+        }
         systemReset();
     }
 
@@ -109,12 +145,12 @@ function applyImpulse() {
     err += impulseStrength;
 }
 
-function systemUpdate() {
+function systemUpdate(dt) {
     err += vel;
-    vel += -intervalSeconds * computeCorrectiveSignal();
+    vel += -dt * computeCorrectiveSignal(dt);
 
     if (biasCheck.checked) {
-        vel += biasSlider.value * maxBias / 100 * intervalSeconds;
+        vel += biasSlider.value * maxBias / 100 * dt;
     }
     
 }
@@ -124,8 +160,8 @@ function systemReset() {
 }
 
 
-function computeCorrectiveSignal() {
-    let der = (err - oldErr) / intervalSeconds;
+function computeCorrectiveSignal(dt) {
+    let der = (err - oldErr) / dt;
     oldErr = err;
     
     let signal = 0;
@@ -133,7 +169,7 @@ function computeCorrectiveSignal() {
         signal += err * getKp();
     }
     if (kICheck.checked) {
-        accErr += err * intervalSeconds * getKi();
+        accErr += err * dt * getKi();
         signal += accErr;
     }
     if (kDCheck.checked) {
